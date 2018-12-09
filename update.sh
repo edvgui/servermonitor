55c90b4b4891e276b3c597c6b3621e5451ba6670
geverartsdev <guillaume.everarts@student.uclouvain.be>
2018-12-09 20:30:06 +0100

    Modified update script

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
+systemctl stop monitor.service
+sleep 5
 git fetch origin master
 git reset --hard origin/master
 chmod +x main.py
-systemctl stop monitor.service
 systemctl daemon-reload
 systemctl start monitor.service
