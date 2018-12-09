8ce00d8805706842c1ca5e63316b0ec77683d77e
geverartsdev <guillaume.everarts@student.uclouvain.be>
2018-12-09 23:58:35 +0100

    Fixing no network error

--- computer.py
+++ computer.py
@@ -44,10 +44,13 @@ class Computer:
         self.lastupdate = int(round(time.time() * 1000))
 
     def update_loc_IP(self):
-        self.localip = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] 
-        if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), 
-        s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, 
-        socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
+        try:
+            self.localip = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] 
+            if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), 
+            s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, 
+            socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
+        except:
+            self.localip = '0.0.0.0'
 
     def update_free_mem(self):
         self.freememory = psutil.virtual_memory().free
